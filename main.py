1499f8d1a30184d246ccf05922f8543d0d6892d1
GJFenn <gregoryjosephfenn@gmail.com>
2021-01-24 10:46:35 +0000

    Simplified main structure + added a mvrc tool (untested).

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os
 import glob
 import shutil
 
-# application constants
-CONSOLE_IO_SYMBOL = ">>> "
-MAX_USER_INPUT_WORDS = 9  # e.g. "ls -a -ifends .c .h" is a list of 5 inputs. The max is this constant value
-NULL_INPUT = "ignore!_!this!_!input"
 
-# application local variables
-__inputs = [NULL_INPUT] * MAX_USER_INPUT_WORDS
+# application constants
+C_CONSOLE_IO_SYMBOL = ">>> "
+C_MAX_USER_INPUT_WORDS = 9  # e.g. "ls -a -ifends .c .h" is a list of 5 inputs. The max is this constant value
+C_NULL_INPUT = "ignore!_!this!_!input"
+
+
+# private static variables
+__inputs = [C_NULL_INPUT] * C_MAX_USER_INPUT_WORDS
 
 
 def applet_default_command():
     pass
 
 
+def fm_help():
+    print("\t'quit' to stop this application, 'help' to show this help page.")
+    print("\tOther supported tools: ls, cprc, mvrc")
+
+
 def unknown_command():
-    print("unknown command")
+    print("Unknown Command")
 
 
 def print_list_by_lines(lst):
@@ -29,7 +36,7 @@ def ls():
 
 
 # Copies files of a specified ending in the filename to the specified directory ("." by default).
-def copy_reccursively():
+def copy_recursively(move=False):
     ends = []
     found_ends = False
     for i in range(len(__inputs)):
@@ -40,75 +47,79 @@ def copy_reccursively():
         print("No -ifends flag found")
         return
     for j in range(i+1, len(__inputs)):
-        if len(__inputs[j]) == 0 or __inputs[j][0] == '-' or __inputs[j] == NULL_INPUT:
+        if len(__inputs[j]) == 0 or __inputs[j][0] == '-' or __inputs[j] == C_NULL_INPUT:
             break
         ends.append(__inputs[j])
+        if __inputs[j] == "*":
+            ends = ['']  # a list of length one, containing the empty string as it's only element
+            break
     if len(ends) == 0:
-        print("No filename endings identified, three examples are .c .txt or .py (the '.' needs to be explicit)")
+        print("ERROR: No filename endings identified, three examples are .c .txt .py (the '.' needs to be explicit).\n"
+              "Or, to affect all subfiles, use *, e.g.   cprc -ifends *    or   mvcprc -ifends *  .")
         return
 
-    src = "."  # copies all files in src and in it's child folders to dst
-    found_dir = False
+    src = "."  # copies all files in src and in its child folders to dst
     for i in range(len(__inputs)):
         if __inputs[i] == "-src":
-            found_dir = True
+            if (i + 1) < len(__inputs):
+                src = __inputs[i + 1]
+            else:
+                print("ERROR: Source directory should be specified after the -src flag.")
+                return
             break
-    if found_dir:
-        if (i+1) < len(__inputs):
-            src = __inputs[i+1]
-        else:
-            print("Source directory should be specified after the -src flag.")
 
     dst = "."  # the copied files will be moved into this directory
-    found_dir = False
     for i in range(len(__inputs)):
         if __inputs[i] == "-dst":
-            found_dir = True
+            if (i + 1) < len(__inputs):
+                dst = __inputs[i + 1]
+            else:
+                print("ERROR: Destination directory should be specified after the -src flag.")
+                return
             break
-    if found_dir:
-        if (i + 1) < len(__inputs):
-            dst = __inputs[i + 1]
-        else:
-            print("Destination directory should be specified after the -src flag.")
     files = []
     for e in ends:
         files += glob.glob(src + "/**/*" + e, recursive=True)
     for file in files:
-        shutil.copy(file, dst)
+        if not move:
+            shutil.copy(file, dst)
+        else:
+            shutil.move(file, dst)
 
 
-# dictionary, input is a one-word, lower-case string
-applet_switch = {
+# moves files of a specified ending in the filename to the specified directory ("." by default).
+def move_recursively():
+    copy_recursively(move=True)
+
+
+applet_switch = {  # dictionary, input is a one-word, lower-case string
     "": applet_default_command,
     "exit": quit,
     "quit": quit,
+    "help": fm_help,
     "ls": ls,
-    "cprc": copy_reccursively,
+    "cprc": copy_recursively,
+    "mvrc": move_recursively,
 }
 
 
 def reset_inputs():
-    for i in range(MAX_USER_INPUT_WORDS):
-        __inputs[i] = NULL_INPUT
+    for i in range(C_MAX_USER_INPUT_WORDS):
+        __inputs[i] = C_NULL_INPUT
 
 
 def main():
     print("\n    **** Running FMTools *****\n")
     while True:
-        user_command = input(CONSOLE_IO_SYMBOL).strip()
+        user_command = input(C_CONSOLE_IO_SYMBOL).strip()
         reset_inputs()
-        if len(user_command) > 0:
-            ucs = user_command.split()
-            applet = ucs[0].lower()            # first word is the applet mode
+        if len(user_command) > 0:  # skip empty inputs
+            ucs = user_command.split()  # list of input parameters (seperated by whitespace)
             for i in range(len(ucs)):
                 __inputs[i] = ucs[i]
-        else:  # no input, skip onwards
-            continue
-
-        __inputs[0] = applet
-        applet_switch.get(__inputs[0], unknown_command)()
+            __inputs[0] = ucs[0].lower()                        # first word is the applet mode
+            applet_switch.get(__inputs[0], unknown_command)()   # run the chosen function
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
